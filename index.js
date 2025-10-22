32d3ca1f03c912ff0b7057c37bff4275c2378964
Md sher Ali <mdakramand123@gmail.com>
2025-10-22 22:41:04 +0530

    crud API with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-//ye db conection ka file impot kiya hai
+const express = require('express');
 const dbconn = require('./mongodbConn');
-//data read karne ka code
-const main = async ()=>{
+const mongodb = require('mongodb');
+const app = express();
+
+//body se data json main convert karega
+app.use(express.json());
+
+//read data
+app.get('/', async (req, res)=>{
   let db = await dbconn();
   data = await db.find().toArray();
   console.log(data);
-}
+  res.send(data);
+});
 
-main();
+//insert
+app.post('/', async (req, res)=>{
+  const db = await dbconn();
+  const result = await db.insertOne(req.body);
+  if(result.acknowledged){
+    res.send({status: 200, message:'insert success'});
+  }
+  
+});
+// update
+app.put('/:name', async (req, res)=>{
+  const db = await dbconn();
+  const result = await db.updateOne({name: req.params.name},{$set: req.body});
+  if(result.modifiedCount > 0){
+    res.send({status: 200, message:'update success'});
+  }else if(result.matchedCount > 0){
+    res.send({status: 200, message:'alredy updated'});
+  }else{
+    res.send({status: 404, message:'no record found'});
+  }
+  
+});
+
+//delete
+app.delete('/:id', async (req, res)=>{
+  const db = await dbconn();
+  const result = await db.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+  if(result.deletedCount > 0){
+    res.send({status: 200, message:'delete success'});
+  }else{
+    res.send({status: 404, message:'no record'});
+  }
+});
+
+app.listen(5000);
