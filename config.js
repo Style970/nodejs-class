853aa7b5f19d9d8cbad5111233db21ec9724fc89
Md sher Ali <mdakramand123@gmail.com>
2025-10-23 16:05:28 +0530

    file upload with mongoose in nodejs

--- config.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://hinakhatoonsk_db_user:lMMNE5XVUSJGPtts@sherali.z1funb0.mongodb.net/?retryWrites=true&w=majority&appName=sherAli");
--- index.js
+++ index.js
@@ -1,53 +1,22 @@
 const express = require('express');
-require("./config");
-const Product = require('./product');
+const multer = require('multer');
+
 const app = express();
 
-// strung ko json main convert karega
-app.use(express.json());
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads')
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    })
+    //ye wahi name dena hai jo postman main hoga
+}).single('file_name');
 
-app.post("/create", async (req, resp) => {
-    let data = new Product(req.body);
-    const result = await data.save();
-    resp.send(result);
+app.post("/upload", upload, (req, resp) => {
+    resp.send("file uploaded...")
 });
-
-app.get("/list", async (req, resp) => {
-    let data = await Product.find();
-    resp.send(data);
-})
-
-app.delete("/delete/:_id", async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.deleteOne(req.params);
-    resp.send(data);
-})
-
-
-app.put("/update/:_id",async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.updateOne(
-        req.params,
-        {$set: req.body}
-    );
-    resp.send(data);
-})
-
-//search
-app.get("/search/:key", async (req, resp) => {
-    console.log(req.params.key);
-    let data = await Product.find(
-      {
-        "$or":[
-          //ek fild lagaoge to single fild main kaam karega
-          {"name":{$regex:req.params.key}},
-          {"brand":{$regex:req.params.key}}
-          ]
-      }
-      );
-      
-    resp.send(data);
-})
-
 
 app.listen(5000)
