458cef7c9e9a182f2543fb4e6d32b6f2f7c01593
Md sher Ali <mdakramand123@gmail.com>
2025-10-23 20:10:49 +0530

    mysql and nodejs crud opration api

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const express = require('express');
-const EventEmitter = require('events');
+const express = require("express");
+const con = require("./config");
 const app = express();
-const event = new EventEmitter();
 
-let num = 0;
-event.on('countApi', ()=>{
-  num++;
-  console.log('event call ',num);
+app.use(express.json());
+
+app.get("/", (req, resp) => {
+  con.query("select * from student", (err, result) => {
+    if (err) { resp.send("error in api") }
+    else { resp.send(result) }
+  })
 });
 
-app.get('/', (req,res)=>{
-  res.send('home api call');
-  //event generate kiya hai
-  event.emit('countApi');
+//insert
+app.post('/', (req,resp)=>{
+  const data= req.body;
+  con.query("INSERT INTO student SET ?",data,(error,result,fields)=>{
+    if(error) throw error;
+    resp.send(result);
+  })
 });
 
-app.get('/list', (req,res)=>{
-  res.send('list api call');
+//update
+app.put("/:id",(req,resp)=>{
+  const data= [req.body.name,req.body.email,req.body.password,req.params.id];
+  con.query("UPDATE student SET name = ?, email = ?, password = ? WHERE id = ?",
+  data,(error,results,fields)=>{
+    if(error) throw error;
+    resp.send(results)
+  });
 });
 
-app.get('/update', (req,res)=>{
-  res.send('update api call');
-    //event generate kiya hai
-  event.emit('countApi');
+//delete
+app.delete("/:id",(req,resp)=>{
+  con.query("DELETE FROM student WHERE id ="+req.params.id,(error,results,fields)=>{
+    if(error) throw error;
+    resp.send(results)
+  });
 });
 
-app.listen(5000);
+app.listen("5000");
